8aeb099e41d32f35068f2ac4a1934180dec754ab
thegostisdead <thegostisdead@gmail.com>
2023-10-21 17:09:37 +0200

    feat: use queue event to display jobs

--- c2-server/backend/src/builder/build-consumer.ts
+++ c2-server/backend/src/builder/build-consumer.ts
@@ -1,15 +1,21 @@
 import { Job } from 'bull';
-import { Process, Processor } from '@nestjs/bull';
+import {OnQueueActive, Process, Processor} from '@nestjs/bull';
 
 @Processor('build')
 export class BuildConsumer {
     @Process('build-job')
-
     handleBuild(job: Job) {
         const { machineData } = job.data;
 
-        console.log('Start build for machine: ', machineData);
+        console.log('Start build...');
         console.log('completed!!');
         // TODO run build command
+    }
+
+    @OnQueueActive()
+    onActive(job: Job) {
+        console.log(
+            `Processing job ${job.id} of type ${job.name} with data ${job.data}...`,
+        );
     }
 }
